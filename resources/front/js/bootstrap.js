ca62501593829a07f97076d25b570fa3ef1f33d2
Ali <ms.kinect@yahoo.com>
2020-01-05 15:48:32 +0330

    delete unnecessary languages from hljs

--- resources/front/js/bootstrap.js
+++ resources/front/js/bootstrap.js
@@ -10,11 +10,22 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-    require('es6-promise/auto');
+    // require('es6-promise/auto');
     require('sticky-kit/dist/sticky-kit.js');
-    window.hljs = require('highlight.js');
+
+    window.hljs = require("highlight.js/lib/highlight.js");
     require('highlightjs-line-numbers.js');
-    require('highlight.js/styles/gruvbox-dark.css');
+    require('highlight.js/styles/gruvbox-light.css');
+    // require('highlight.js/styles/gruvbox-dark.css');
+    hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'));
+    hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
+    hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+    hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+    hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+    hljs.registerLanguage('plaintext', require('highlight.js/lib/languages/plaintext'));
+    hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
+    hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+
     require('./custom.js');
     window.Cookies = require('js-cookie');
     window.slick = require('slick-carousel');
